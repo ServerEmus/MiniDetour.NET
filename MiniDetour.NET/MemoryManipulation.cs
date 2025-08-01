c48d181b0fe59731314805a0bf374fa160dbd1fe
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-08-01 11:24:57 +0200

    implemented all stuff

--- MiniDetour.NET/MemoryManipulation.cs
+++ MiniDetour.NET/MemoryManipulation.cs
@@ -33,7 +33,6 @@ public static partial class MemoryManipulation
     )
         => MemoryManipulationNative.SafeMemoryRead(address, buffer, size);
     
-    // This need test.
     public static bool SafeMemoryRead(
         IntPtr address,
         byte[] buffer,
@@ -41,10 +40,41 @@ public static partial class MemoryManipulation
     )
     {
         int isize = (int)size;
-        IntPtr bufferptr = Marshal.AllocHGlobal(isize);
-        bool ret = SafeMemoryRead(address, bufferptr, size);
-        Marshal.Copy(bufferptr, buffer, 0, isize);
-        Marshal.FreeHGlobal(bufferptr);
+        GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        bool ret = SafeMemoryRead(address, handle.AddrOfPinnedObject(), size);
+        handle.Free();
         return ret;
     }
+
+        public static bool SafeMemoryWrite(
+        IntPtr address,
+        IntPtr buffer,
+        UIntPtr size
+    )
+        => MemoryManipulationNative.SafeMemoryWrite(address, buffer, size);
+    
+    public static bool SafeMemoryWrite(
+        IntPtr address,
+        byte[] buffer,
+        UIntPtr size
+    )
+    {
+        int isize = (int)size;
+        GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        bool ret = SafeMemoryWrite(address, handle.AddrOfPinnedObject(), size);
+        handle.Free();
+        return ret;
+    }
+
+    public static UIntPtr WriteAbsoluteJump(
+        IntPtr address,
+        IntPtr destination
+    )
+        => MemoryManipulationNative.WriteAbsoluteJump(address, destination);
+
+    public static int FlushInstructionCache(
+        IntPtr address,
+        UIntPtr size
+    )
+        => MemoryManipulationNative.FlushInstructionCache(address, size);
 }
--- MiniDetour.NET/ModuleManipulation.cs
+++ MiniDetour.NET/ModuleManipulation.cs
@@ -5,5 +5,77 @@ namespace MiniDetour;
 
 public static partial class ModuleManipulation
 {
-    
+    public static UIntPtr GetAllExportedSymbols(
+        IntPtr moduleHandle,
+        UIntPtr exportDetailsCount,
+        out ExportDetails[] exportDetails
+    )
+    {
+        exportDetails = new[(int)exportDetailsCount];
+        return ModuleManipulationNative.GetAllExportedSymbols(moduleHandle, exportDetails, exportDetailsCount);
+    }
+
+    public static UIntPtr GetAllIATSymbols(
+        IntPtr moduleHandle,
+        UIntPtr iatDetailsCount,
+        out IATDetails[] iatDetails
+    )
+    {
+        iatDetails = new[(int)iatDetailsCount];
+        return ModuleManipulationNative.GetAllIATSymbols(moduleHandle, iatDetails, iatDetailsCount);
+    }
+
+    public static UIntPtr ReplaceModuleExports(
+        IntPtr moduleHandle,
+        ExportReplaceParameter[] exportReplaceDetails,
+        UIntPtr exportReplaceDetailsCount
+    )
+        => ModuleManipulationNative.ReplaceModuleExports(moduleHandle, exportReplaceDetails, exportReplaceDetailsCount);
+
+    public static UIntPtr ReplaceModuleExports(
+        IntPtr moduleHandle,
+        List<ExportReplaceParameter> exportReplaceDetails,
+    )
+        => ReplaceModuleExports(moduleHandle, exportReplaceDetails.ToArray(), (UIntPtr)exportReplaceDetails.Count);
+
+    public static UIntPtr RestoreModuleExports(
+        IntPtr moduleHandle,
+        ExportReplaceParameter[] exportReplaceDetails,
+        UIntPtr exportReplaceDetailsCount
+    )
+        => ModuleManipulationNative.RestoreModuleExports(moduleHandle, exportReplaceDetails, exportReplaceDetailsCount);
+
+    public static UIntPtr RestoreModuleExports(
+        IntPtr moduleHandle,
+        List<ExportReplaceParameter> exportReplaceDetails,
+    )
+        => RestoreModuleExports(moduleHandle, exportReplaceDetails.ToArray(), (UIntPtr)exportReplaceDetails.Count);
+
+    public static UIntPtr ReplaceModuleIATs(
+        IntPtr moduleHandle,
+        IATReplaceParameter[] iatReplaceDetails,
+        UIntPtr iatReplaceDetailsCount
+    )
+        => ModuleManipulationNative.ReplaceModuleIATs(moduleHandle, iatReplaceDetails, iatReplaceDetailsCount);
+
+    public static UIntPtr ReplaceModuleIATs(
+        IntPtr moduleHandle,
+        List<IATReplaceParameter> iatReplaceDetails,
+    )
+        => ReplaceModuleIATs(moduleHandle, exportReplaceDetails.ToArray(), (UIntPtr)exportReplaceDetails.Count);
+
+    public static UIntPtr RestoreModuleIATs(
+        IntPtr moduleHandle,
+        IATReplaceParameter[] iatReplaceDetails,
+        UIntPtr iatReplaceDetailsCount
+    )
+        => ModuleManipulationNative.RestoreModuleIATs(moduleHandle, iatReplaceDetails, iatReplaceDetailsCount);
+
+    public static UIntPtr RestoreModuleIATs(
+        IntPtr moduleHandle,
+        List<IATReplaceParameter> iatReplaceDetails,
+    )
+        => RestoreModuleIATs(moduleHandle, iatReplaceDetails.ToArray(), (UIntPtr)iatReplaceDetails.Count);
+
+
 }
