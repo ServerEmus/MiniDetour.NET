2a8dd19da31f2fcc3ec03305cdc7ca98511c2992
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-08-04 14:48:28 +0200

    sgdfg

--- MiniDetour.NET.Example/Example.cs
+++ MiniDetour.NET.Example/Example.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using MiniDetour;
 
 public unsafe class Program
